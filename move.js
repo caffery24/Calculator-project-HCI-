2922254c3caf96a6e5ff47a19452512ea4e0f906
caffery <caffery@dhcp-10-105-234-231.wireless.northwestern.private>
2016-02-26 20:56:31 -0600

    Caculator

---
+++ move.js
@@ -0,0 +1,157 @@
+var zero=0;
+var decimal=false;
+var hasnumber=false;
+var neg=true;
+var firstnum=false;
+var pl=false;
+var mu=false;
+var di=false;
+var mi=false;
+
+var first=0;
+var second=0;
+var result=0;
+
+var op=document.getElementById("operend");
+var C1=document.getElementById("C");
+var num=document.getElementById("number");
+var dec=document.getElementById("decimal");
+var pm=document.getElementById("pm");
+var plus=document.getElementById("+");
+var minus=document.getElementById("-");
+var mul=document.getElementById("*");
+var div=document.getElementById("/")	;
+var equal=document.getElementById("e");
+
+
+C1.onclick=function(event){
+	num.innerHTML=zero;
+	hasnumber=false;
+	decimal=false;
+	op.innerHTML="";
+	firstnum=false;
+	neg=false;
+}; 
+
+for(var i=0;i<10;i++){
+	document.getElementById("n"+i).onclick=function(event){
+		if(!hasnumber){
+			num.innerHTML="";
+			hasnumber=true;
+		}
+			num.innerHTML+=event.target.id.substring(1);
+	};
+}
+
+dec.onclick=function(event){
+	if(!decimal){
+		num.innerHTML+=".";
+		decimal=true;
+	}
+}
+
+pm.onclick=function(event){
+	if(hasnumber){
+		if(neg)
+		num.innerHTML="-"+num.innerHTML;	
+		else
+		num.innerHTML=num.innerHTML.substring(1);
+		neg=!neg;
+	}
+}
+
+plus.onclick=function(evnet){
+	if(!firstnum){
+		first=parseFloat(num.innerHTML);
+		firstnum=true;
+		hasnumber=false;
+	}
+	else{
+		second=parseFloat(num.innerHTML);
+		result=first+second;
+		num.innerHTML=result;
+		hasnumber=false;
+		first=result;
+	}
+	op.innerHTML="+";
+	pl=true;
+}
+
+minus.onclick=function(evnet){
+	if(!firstnum){
+		first=parseFloat(num.innerHTML);
+		firstnum=true;
+		hasnumber=false;
+	}
+	else{
+		second=parseFloat(num.innerHTML);
+		result=first-second;
+		num.innerHTML=result;
+		hasnumber=false;
+		first=result;
+	}
+	mi=true;
+	op.innerHTML="-";
+}
+
+mul.onclick=function(evnet){
+	if(!firstnum){
+		first=parseFloat(num.innerHTML);
+		firstnum=true;
+		hasnumber=false;
+	}
+	else{
+		second=parseFloat(num.innerHTML);
+		result=first*second;
+		num.innerHTML=result;
+		hasnumber=false;
+		first=result;
+	}
+	mu=true;
+	op.innerHTML="x";
+}
+
+div.onclick=function(evnet){
+	if(!firstnum){
+		first=parseFloat(num.innerHTML);
+		firstnum=true;
+		hasnumber=false;
+	}
+	else{
+		second=parseFloat(num.innerHTML);
+		result=first/second;
+		num.innerHTML=result;
+		hasnumber=false;
+		first=result;
+	}
+	di=true;
+	op.innerHTML="/";
+}
+
+equal.onclick=function(event){
+		second=parseFloat(num.innerHTML);
+		if(pl){
+			num.innerHTML=first+second;
+			pl=false;
+		}
+		else if(mu){
+			num.innerHTML=first*second;
+			mu=false;
+		}
+		else if(di){
+			if(second==0)
+				num.innerHTML="not a number";
+			else
+			num.innerHTML=first/second;
+			di=false;
+		}
+		else if(mi){
+			num.innerHTML=first-second;
+			mi=false;
+		}
+		op.innerHTML="";
+		hasnumber=false;	
+		firstnum=false;	
+		decimal=false;
+}
+
